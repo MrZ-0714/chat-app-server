7e63cd1bc79482a21b85f3e12cbf63ef3eadb2db
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-03-24 03:03:31 -0700

    connect to mongodb

--- express-server.js
+++ express-server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = 8080;
 
 app.listen(port, () => {
@@ -11,4 +12,11 @@ app.get("/", (req, res) => res.send("Ha"));
 app.get("/api/hey", (req, res) => {
   console.log(req);
   res.send("ho!");
+});
+
+await mongoose.connect('mongodb://localhost/my_database', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 });
