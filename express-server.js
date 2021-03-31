2e95b1aab8261450024cd6f0a45da0524730b935
MrZ-0714 <zimo.zhao.0714@gmail.com>
2021-03-31 14:49:36 -0700

    remove mongodb for now, config socket.io

--- express-server.js
+++ express-server.js
@@ -1,32 +1,36 @@
-import express from "express";
-import mongoose from "mongoose";
+const express = require("express");
+const socket = require("socket.io");
+
 const app = express();
 const port = 8080;
-
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log("Express server is listening on port: ", port);
 });
+//Static files
+app.use(express.static("public"));
 
 app.get("/", (req, res) => res.send("Ha"));
 
 app.get("/api/hey", (req, res) => {
-  console.log(req);
   res.send("ho!");
 });
 
-mongoose.connect("mongodb://localhost:27017/newTestDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", () => {
-  console.log("Connected to mongodb");
-  const kittySchema = new mongoose.Schema({ name: String });
-  const Kitten = mongoose.model("kitten", kittySchema);
-  const silence = new Kitten({ name: "silence" });
-  // silence.save((err, silence) => console.log(err, silence));
-  
-});
+const io = socket(server);
+io.on("connection", (socket) => console.log("Made socket connection", socket));
+
+// import mongoose from "mongoose";
+// mongoose.connect("mongodb://localhost:27017/newTestDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+// });
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", () => {
+//   console.log("Connected to mongodb");
+//   const kittySchema = new mongoose.Schema({ name: String });
+//   const Kitten = mongoose.model("kitten", kittySchema);
+//   const silence = new Kitten({ name: "silence" });
+//   // silence.save((err, silence) => console.log(err, silence));
+// });
